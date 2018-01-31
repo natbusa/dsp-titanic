6d353efeaca0a8969b120d00d84f1b6e7c572087
natbusa <natalino.busa@gmail.com>
2018-01-31 15:21:39 +0800

    added basic ci script

---
+++ ci/scripts/build-image.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+URL=$(git remote show -n origin | grep Fetch | cut -d: -f2-)
+IMAGE=$(basename $URL .git)
+VERSION=$(git rev-parse --short HEAD)
+
+CMD="repo2docker --no-run --debug --user-id 1000 --user-name jovyan --image $IMAGE:$VERSION ${URL%.git}"
+docker run -v /var/run/docker.sock:/var/run/docker.sock jupyter/repo2docker:8a5b1ef $CMD
