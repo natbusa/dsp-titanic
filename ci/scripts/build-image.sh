b14de01a61dba97f7f5d9d436a489d7f575bbfc7
natbusa <natalino.busa@gmail.com>
2018-01-31 22:32:25 +0800

    building image, push to local repo

--- ci/scripts/build-image.sh
+++ ci/scripts/build-image.sh
@@ -4,9 +4,6 @@ URL=$(git remote show -n origin | grep Fetch | cut -d: -f2-)
 IMAGE=$(basename $URL .git)
 VERSION=$(git rev-parse --short HEAD)
 
-jupyter-repo2docker --no-run --no-build --debug --user-id 1000 --user-name jovyan --image $IMAGE:$VERSION ${URL%.git} | tee Dockerfile
-pwd
-ls -al
-
-#echo $CMD
-#docker run -v /var/run/docker.sock:/var/run/docker.sock jupyter/repo2docker:8a5b1ef $CMD
+jupyter-repo2docker --no-run --no-build --debug --user-id 1000 --user-name jovyan --image $IMAGE:$VERSION . 2> dockerfile.tmp
+sed -n -e '/FROM/,$p' dockerfile.tmp > Dockerfile
+rm dockerfile.tmp
