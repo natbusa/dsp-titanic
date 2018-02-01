41cf93b48487a3445ffaf6414f915306a8cf5792
natbusa <natalino.busa@gmail.com>
2018-02-01 15:39:52 +0800

    fixed docker build_args

--- ci/scripts/build-image.sh
+++ ci/scripts/build-image.sh
@@ -5,8 +5,9 @@ URL=$(git remote show -n origin | grep Fetch | cut -d: -f2-)
 IMAGE=$(basename $URL .git)
 VERSION=$(git rev-parse --short HEAD)
 
-jupyter-repo2docker --no-run --no-build --debug --user-id 1000 --user-name jovyan --image $IMAGE:$VERSION . 2> dockerfile.tmp
+jupyter-repo2docker --no-build --debug --user-id 1000 --user-name jovyan --image $IMAGE:$VERSION . 2> dockerfile.tmp
 sed -n -e '/FROM/,$p' dockerfile.tmp > Dockerfile
+cat Dockerfile
 rm dockerfile.tmp
 popd
 
