84778e9b9466f53c1a1d8a411b7fcee98370a12f
natbusa <natalino.busa@gmail.com>
2018-02-06 00:37:58 +0800

    fixed directories mapping

--- ci/scripts/build-image.sh
+++ ci/scripts/build-image.sh
@@ -6,22 +6,17 @@ IMAGE=$(basename $URL .git)
 VERSION=$(git rev-parse --short HEAD)
 
 jupyter-repo2docker --no-build --debug --user-id 1000 --user-name jovyan --image $IMAGE:$VERSION . 2> dockerfile.tmp
-sed -n -e '/FROM/,$p' dockerfile.tmp > Dockerfile
-cat Dockerfile
+sed -n -e '/FROM/,$p' dockerfile.tmp > build/Dockerfile
 rm dockerfile.tmp
 popd
 
-#copy files to output dir build
-cp -Ra repo/binder build
-cp -Ra repo/src build || :
-cp repo/Dockerfile build
-cp repo/README.md  build
+#copy files to output dir build/src
+cp -Ra repo build/src
 
 R2D_PATH=/usr/local/lib/python3.6/site-packages/repo2docker
 cp -Ra $R2D_PATH/buildpacks/conda build
 cp -Ra $R2D_PATH/buildpacks/python build
 
-# src directory is required when building even if empty
-mkdir -p build/src
-
-ls -la build
+# debug
+cat build/Dockerfile
+ls -Rla build
