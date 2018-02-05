9a1d6709a60ee328d31f741b8b4648fc8f8c4ad3
natbusa <natalino.busa@gmail.com>
2018-02-06 00:47:18 +0800

    correct Dockerfile location

--- ci/scripts/build-image.sh
+++ ci/scripts/build-image.sh
@@ -6,16 +6,18 @@ IMAGE=$(basename $URL .git)
 VERSION=$(git rev-parse --short HEAD)
 
 jupyter-repo2docker --no-build --debug --user-id 1000 --user-name jovyan --image $IMAGE:$VERSION . 2> dockerfile.tmp
-sed -n -e '/FROM/,$p' dockerfile.tmp > build/Dockerfile
+sed -n -e '/FROM/,$p' dockerfile.tmp > ../Dockerfile
 rm dockerfile.tmp
 popd
 
 #copy files to output dir build/src
 cp -Ra repo build/src
 
+# copy/move build packs and Dockerfiles
 R2D_PATH=/usr/local/lib/python3.6/site-packages/repo2docker
 cp -Ra $R2D_PATH/buildpacks/conda build
 cp -Ra $R2D_PATH/buildpacks/python build
+mv Dockerfile build/
 
 # debug
 cat build/Dockerfile
