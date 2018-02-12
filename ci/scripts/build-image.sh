888578855262d7f6f2a13e9f4e858a2bb038a269
Natalino Busa <natalino.busa@gmail.com>
2018-02-12 17:24:48 +0800

    Update build-image.sh

--- ci/scripts/build-image.sh
+++ ci/scripts/build-image.sh
@@ -10,6 +10,7 @@ REPO_BRANCH="$(git branch | grep \* | cut -d ' ' -f2)"
 REPO_VERSION=$(git rev-parse --short HEAD)
 
 IMAGE_NAME="$(echo ${REPO_PATH%.git} $REPO_BRANCH | tr -c [a-zA-Z0-9-_] - | sed '$s/.$//' )"
+echo building image $IMAGE_NAME:$REPO_VERSION 
 
 jupyter-repo2docker --no-build --debug --user-id 1000 --user-name jovyan --image $IMAGE_NAME:$REPO_VERSION . 2> dockerfile.tmp
 sed -n -e '/FROM/,$p' dockerfile.tmp > ../Dockerfile
